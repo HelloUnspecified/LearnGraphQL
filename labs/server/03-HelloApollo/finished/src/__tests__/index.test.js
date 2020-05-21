17b0426b46a04bc4cd6e52a673904f7bf7be2619
Clark Sell <clark.sell@csell.net>
2020-05-21 09:51:20 -0500

    moar testing

--- labs/server/03-HelloApollo/finished/src/__tests__/index.test.js
+++ labs/server/03-HelloApollo/finished/src/__tests__/index.test.js
@@ -1,14 +1,50 @@
 const request = require('supertest');
+const { gql } = require('apollo-server-express');
 
 const app = require('../index');
 
 describe('playground', () => {
   it('can get playground', (done) => {
     request(app)
-      .get('/graphql')
+      .get('/')
       .set('Accept', 'text/html')
       .then((response) => {
         expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+});
+
+describe('graphql server', () => {
+  it('get raw query', (done) => {
+    const rawPostBody = { query: 'query {hello}' };
+    request(app)
+      .get(`/?query=${rawPostBody.query}`)
+      .set('Accept', 'application/json')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual({
+          data: {
+            hello: 'world',
+          },
+        });
+        done();
+      });
+  });
+
+  it('post raw query', (done) => {
+    const rawPostBody = { query: 'query {hello}' };
+    request(app)
+      .post('/')
+      .set('Accept', 'application/json')
+      .send(rawPostBody)
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual({
+          data: {
+            hello: 'world',
+          },
+        });
         done();
       });
   });
