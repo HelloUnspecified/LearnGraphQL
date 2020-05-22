b73df97093c156120df06c610bdbafbd778589d8
Clark Sell <clark.sell@csell.net>
2020-05-22 09:55:25 -0500

    graphql file conversion

---
+++ labs/server/05-typedefs/finished/src/__tests__/index.test.js
@@ -0,0 +1,33 @@
+const request = require('supertest');
+
+const app = require('../index');
+
+describe('playground', () => {
+  it('can get playground', (done) => {
+    request(app)
+      .get('/')
+      .set('Accept', 'text/html')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
+});
+
+describe('graphql server', () => {
+  it('get raw query', (done) => {
+    const rawPostBody = { query: 'query {hello}' };
+    request(app)
+      .get(`/?query=${rawPostBody.query}`)
+      .set('Accept', 'application/json')
+      .then((response) => {
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual({
+          data: {
+            hello: 'world',
+          },
+        });
+        done();
+      });
+  });
+});
